99fd17d6e0784b3de7f165ba55d05075845b8837
Aminu Rabiu <arabiu033@gmail.com>
2023-01-06 01:05:41 +0100

    Refactor the city and state class to inherit from the base class and support DB

--- models/state.py
+++ models/state.py
@@ -1,8 +1,23 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
+from sqlalchemy import ForeignKey
+from sqlalchemy.orm import relationship
 
 
-class State(BaseModel):
+class State(BaseModel, Baase):
     """ State class """
-    name = ""
+        __tablename__ = 'states'
+    name = Column(String(128), nullable=False)
+    cities = relationship('City', backref='state', cascade='delete')
+
+    if ("HBNB_TYPE_STORAGE", None) is None:
+        from models import storage
+        @property
+        def cities(self):
+            Clist = []
+            for city in list(storage.all(City).values()):
+                if city.state_id == self.id:
+                    city_list.append(city)
+            return Clist
