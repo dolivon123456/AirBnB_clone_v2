4bf4326365d58d995832814ea9b4eff2ffb6e304
Aminu Rabiu <arabiu033@gmail.com>
2023-01-06 01:34:23 +0100

    Fix bug

--- models/__init__.py
+++ models/__init__.py
@@ -2,6 +2,13 @@
 """This module instantiates an object of class FileStorage"""
 from models.engine.file_storage import FileStorage
 from os import getenv
+from models.base_model import BaseModel
+from models.user import User
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
 
 if getenv("HBNB_TYPE_STORAGE") == "db":
     from models.engine.db_storage import DBStorage
@@ -9,5 +16,5 @@ if getenv("HBNB_TYPE_STORAGE") == "db":
 else:
     from models.engine.file_storage import FileStorage
     storage = FileStorage()
-    
+
 storage.reload()
--- models/base_model.py
+++ models/base_model.py
@@ -20,8 +20,7 @@ class BaseModel:
         """Instatntiates a new model"""
         if not kwargs:
             self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
+            self.updated_at = self.created_at = datetime.now()
         else:
             for key, value in kwargs.items():
                 if key == "created_at" or key == "updated_at":
@@ -49,8 +48,7 @@ class BaseModel:
         """Convert instance into dict format"""
         dictionary = {}
         dictionary.update(self.__dict__)
-        dictionary.update({'__class__':
-                          (str(type(self)).split('.')[-1]).split('\'')[0]})
+        my_dict["__class__"] = str(type(self).__name__)
         dictionary['created_at'] = self.created_at.isoformat()
         dictionary['updated_at'] = self.updated_at.isoformat()
         my_dict.pop("_sa_instance_state", None)
