6e85ff508c0e492f4290131accca38531b3ff6ff
Aminu Rabiu <arabiu033@gmail.com>
2023-01-06 03:30:37 +0100

    update

--- models/user.py
+++ models/user.py
@@ -20,5 +20,5 @@ class User(BaseModel, Base):
     first_name = Column(String(128), nullable=True)
     last_name = Column(String(128), nullable=True)
 
-    reviews = relationship('Review', cascade='all, delete', backref='user')
-    places = relationship('Place', cascade='all, delete', backref='user')
+    reviews = relationship('Review', cascade='delete', backref='user')
+    places = relationship('Place', cascade='delete', backref='user')
