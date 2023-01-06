95a6cc64fcd33bdb1235564a33c6049de18de777
Aminu Rabiu <arabiu033@gmail.com>
2023-01-06 02:16:58 +0100

    restructure the state class

--- models/state.py
+++ models/state.py
@@ -1,24 +1,37 @@
 #!/usr/bin/python3
-""" State Module for HBNB project """
+"""This is the state class"""
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
-from sqlalchemy import ForeignKey
 from sqlalchemy.orm import relationship
+from os import environ
 
 
 class State(BaseModel, Base):
-    """ State class """
+    """This is the class for State
+    Attributes:
+        __tablename__: name of MySQL table
+        name: input name
+    """
     __tablename__ = 'states'
     name = Column(String(128), nullable=False)
-    cities = relationship('City', backref='state', cascade='all, delete')
 
-    if ("HBNB_TYPE_STORAGE", None) is None:
-        from models import storage
-        from models.city import City
+    if environ['HBNB_TYPE_STORAGE'] == 'db':
+        cities = relationship('City', cascade='all, delete', backref='state')
+    else:
         @property
         def cities(self):
-            Clist = []
-            for city in list(storage.all(City).values()):
+            """Getter method for cities
+            Return: list of cities with state_id equal to self.id
+            """
+            from models import storage
+            from models.city import City
+            # return list of City objs in __objects
+            cities_dict = storage.all(City)
+            cities_list = []
+
+            # copy values from dict to list
+            for city in cities_dict.values():
                 if city.state_id == self.id:
-                    city_list.append(city)
-            return Clist
+                    cities_list.append(city)
+
+            return cities_list
