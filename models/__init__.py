5ad1978d9cfc9cc3c958c0fd94942abba09ba211
Aminu Rabiu <arabiu033@gmail.com>
2023-01-07 01:50:24 +0100

    Handled a bug in the __init__ file

--- models/__init__.py
+++ models/__init__.py
@@ -1,10 +1,4 @@
 #!/usr/bin/python3
-"""This module instantiates an object of class FileStorage"""
-from models.engine.file_storage import FileStorage
-
-
-storage = FileStorage()
-storage.reload()#!/usr/bin/python3
 """create a unique FileStorage instance for your application
 """
 from models.engine.file_storage import FileStorage
